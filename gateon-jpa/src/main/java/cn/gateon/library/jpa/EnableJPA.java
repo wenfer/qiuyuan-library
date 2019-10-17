6f7fe61ec2545d8fe7bf9f4a1d5f6c6ea586edfc
qiuyuan <wleoi@qq.com>
2019-10-17 16:08:59 +0800

    1.0  snapshot version

--- gateon-jpa/src/main/java/cn/gateon/library/jpa/EnableJPA.java
+++ gateon-jpa/src/main/java/cn/gateon/library/jpa/EnableJPA.java
@@ -1,8 +1,20 @@
 package cn.gateon.library.jpa;
 
+import cn.gateon.library.jpa.repo.GateonRepositoryImpl;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.lang.annotation.*;
+
 /**
  * @author qiuyuan
  * @since 1.0
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@EnableJpaRepositories(
+        value = {"**.repository.**", "**.model.**"},
+        repositoryBaseClass = GateonRepositoryImpl.class)
 public @interface EnableJPA {
 }
