4d1a2bba4afcf11693207ac7ca083c42fa3c9c84
qiuyuan <wleoi@qq.com>
2020-04-09 13:54:38 +0800

    join类型修改

--- gateon-jpa/src/main/java/cn/gateon/library/jpa/core/query/BaseQuery.java
+++ gateon-jpa/src/main/java/cn/gateon/library/jpa/core/query/BaseQuery.java
@@ -110,7 +110,7 @@ public abstract class BaseQuery<F, R> implements Queryer<F, R> {
 
     @Override
     public <Z> CollectionQueryer<Z> joinCollection(String property) {
-        Join<F, Z> objectObjectCollectionJoin = root.joinList(property, JoinType.LEFT);
+        Join<F, Z> objectObjectCollectionJoin = root.join(property, JoinType.LEFT);
         return new CollectionQuery<>(objectObjectCollectionJoin, cb);
     }
 
